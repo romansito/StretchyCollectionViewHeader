270ec6456437fba93666899f664e8b7016f7fd4b
Roman Salazar <romansito18@gmail.com>
2016-07-23 10:39:53 -0700

    Initial Commit

---
+++ StrechyHeaderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StrechyHeaderView
+//
+//  Created by Roman Salazar on 7/23/16.
+//  Copyright © 2016 Roman Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StrechyHeaderView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StrechyHeaderView
+//
+//  Created by Roman Salazar on 7/23/16.
+//  Copyright © 2016 Roman Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
